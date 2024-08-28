3b459ad3272017ff26cf98dd844954615489b9d5
faiza <faizachoudhry.282@gmail.com>
2024-08-28 15:12:28 +0500

    last

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, {Component} from "react";
 
-// Element----
-let el = React.createElement("h1", null, "Hello World;")
+// Creating Element----
+let elem = React.createElement("h1", null, "Hello World;");
+export default elem;
 
-// Component----
+// Creating Component----
 class App extends Component {
     render(){
         return React.createElement("h1", null, "Hello world..");
+    //  return <h1>Hello world..</h1>   -----also write this in JSX----
     }
 }
 export default App;
+
+//----Functional Component----without props---
+function Student(){
+    return <h1>Hello React</h1>;
+};
+const Student = () => {
+    return <h1>Hello React</h1>;
+};
+
+//------Function with props---------
+const Student = props => {
+    return <h1>Hello {props.name}</h1>;
+};
+export default Student;
+
+//-------Class Component with props-------
+class Teacher extends Component {
+    render() {
+        <h1>Hello {this.props.name}</h1>;
+    }
+}
+export default Teacher;
+
--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//For Element in App.js---
-import el from "./App";
+//Import for Student Function---
+import Student from "./App";
 
-// For Component in App.js---
+//Import for Element in App.js---
+import elem from "./App";
+
+//Import for Component in App.js---
 import App from "./App";
 
-//----Jsx code---
- let el = <h1>Faiza Choudhry</h1>;  
-  ReactDOM.render(el,document.getElementById("root"));
+//------------------Jsx code--------------------
+let el = <h1>Faiza Choudhry</h1>;  
+ReactDOM.render(el,document.getElementById("root"));
 
-//---Bable----
- let el = React.createElement("h1", null, "Hello World");  
- ReactDOM.render(el,document.getElementById("root"));
+//---------Bable convert Jsx code into this code--------
+let el1 = React.createElement("h1", null, "Hello World");  
+ReactDOM.render(el1,document.getElementById("root"));
 
- // for Componenet in App.js
+ //-----render for Componenet App in App.js-----
 ReactDOM.render(<App />,document.getElementById("root"));
+
+//-----render function without props----
+ReactDOM.render(<Student />,document.getElementById("root"));
+//------render function with props------
+ReactDOM.render(<Student name="React" />,document.getElementById("root"))
+
+//-----render Class Components with props----
+ReactDOM.render(<Teacher name="React" />,document.getElementById("root"));
